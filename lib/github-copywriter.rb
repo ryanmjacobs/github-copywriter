3839ebd73060b7c499c656ac647507a96a8e2f88
Ryan Jacobs <ryan.mjacobs@gmail.com>
2014-12-10 19:27:03 -0800

    auth to github!

--- lib/github-copywriter.rb
+++ lib/github-copywriter.rb
@@ -1,5 +1,19 @@
-require "github-copywriter/version"
+require "highline/import"
+require "octokit"
 
 module Copywriter
-  # Your code goes here...
+    extend self
+
+    VERSION = "0.0.1"
+
+    def login!
+        # Grab username and pass
+        puts "Obtaining OAuth2 access_token from github."
+        username = ask("GitHub username: ") { |q| q.echo = true }
+        password = ask("GitHub password: ") { |q| q.echo = "*" }
+
+        # Auth to GitHub
+        @client = Octokit::Client.new(:login => username, :password => password)
+        @user   = @client.user
+    end
 end
