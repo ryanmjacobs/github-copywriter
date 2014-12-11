df7f291311a7ce99319260ea8e430015fd1c3e65
Ryan Jacobs <ryan.mjacobs@gmail.com>
2014-12-10 20:32:19 -0800

    update_copyright placeholders

--- lib/github-copywriter.rb
+++ lib/github-copywriter.rb
@@ -1,5 +1,6 @@
 require "highline/import"
 require "octokit"
+require "base64"
 
 module Copywriter
     extend self
@@ -21,5 +22,25 @@ module Copywriter
         @user_repos = @client.repositories(:user => @client.login)
     end
 
+    ##
+    # Updates copyright using regex.
+    #
+    #   input base64 encoded file
+    # returns base64 encoded file
+    def update_copyright(base64_input)
+        input = Base64.decode64(base64_input)
+        puts input
+    end
+
+    ##
+    # Loop through each file in each repo and update the copyright.
+    def update_all_copyrights
+        @user_repos.each do |repo|
+            repo = repo["full_name"]
+            ref  = repo["default_branch"]
+        end
+    end
+
+    private :update_copyright
     attr_reader :client
 end
