dccea7e47d9bb44b5f519db7413d408a163db8af
Ryan Jacobs <ryan.mjacobs@gmail.com>
2014-12-14 22:34:13 -0800

    update accepted_name spec

--- spec/accepted_name_spec.rb
+++ spec/accepted_name_spec.rb
@@ -1,18 +1,21 @@
 require "spec_helper"
 
 describe Copywriter do
-    include Copywriter
+    regex = Copywriter::Regex
 
-    it { expect(accepted_name?      "README")  .to be(true) }
-    it { expect(accepted_name?     "LICENSE")  .to be(true) }
-    it { expect(accepted_name?   "README.md")  .to be(true) }
-    it { expect(accepted_name?  "LICENSE.md")  .to be(true) }
-    it { expect(accepted_name? "whatever.md")  .to be(true) }
-    it { expect(accepted_name?   "thing.html") .to be(true) }
-    it { expect(accepted_name? "my_site.html") .to be(true) }
+    # Accepted
+    it { expect(regex.accepted_name?      "README")  .to be(true) }
+    it { expect(regex.accepted_name?     "LICENSE")  .to be(true) }
+    it { expect(regex.accepted_name?   "README.md")  .to be(true) }
+    it { expect(regex.accepted_name?  "LICENSE.md")  .to be(true) }
 
-    it { expect(accepted_name? "sadkfhasdfga") .to be(false) }
-    it { expect(accepted_name?    "colors.py") .to be(false) }
-    it { expect(accepted_name? "list_files.c") .to be(false) }
-    it { expect(accepted_name?       "prog.c") .to be(false) }
+    # Accepted
+    it { expect(regex.accepted_name? "whatever.md")  .to be(true) }
+    it { expect(regex.accepted_name?   "thing.html") .to be(true) }
+
+    # Not Accepted
+    it { expect(regex.accepted_name?      "garbage") .to be(false) }
+    it { expect(regex.accepted_name?    "colors.py") .to be(false) }
+    it { expect(regex.accepted_name? "list_files.c") .to be(false) }
+    it { expect(regex.accepted_name?   "coolLib.js") .to be(false) }
 end
