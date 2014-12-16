802623ee1eda68fe3eb824bc28f5f6ceacdb1a3f
Ryan Jacobs <ryan.mjacobs@gmail.com>
2014-12-15 18:41:09 -0800

    Add --branches and --all-branches.
    Also, validate --year.

--- lib/github-copywriter.rb
+++ lib/github-copywriter.rb
@@ -32,7 +32,13 @@ class Copywriter
 
     def run!(options={})
         # Default options
-        options = {all: false, skip_forks: false, year: nil}.merge(options)
+        options = {
+            all: false,
+            skip_forks: false,
+            year: nil,
+            branches: nil,
+            all_branches: false
+        }.merge(options)
 
         if options[:all] then
             repos = @client.repositories()
@@ -58,56 +64,78 @@ class Copywriter
             # Skip if repo is a fork and --skip-forks is on
             next if options[:skip_forks] and repo[:fork]
 
-            # Get repo info
-            repo_name     = repo[:full_name]
-            ref           = "heads/#{repo[:default_branch]}"
-            latest_commit = @client.ref(repo_name, ref).object
-            root_tree     = @client.commit(repo_name, latest_commit.sha).commit.tree
-            puts "\n"+repo_name+":"
+            # Repo name
+            repo_name = repo[:full_name]
+            puts "\n#{repo_name}:".cyan
 
-            # Grab the *whole* tree
-            tree = @client.tree(repo_name, root_tree.sha, :recursive => true)
-
-            # warn user about truncation
-            if tree[:truncated] then
-                puts "  warning: tree truncated because number of items exceeded limit.".yellow
-                puts "           If you feel like fixing this, see issue #xx".yellow
-                puts "           http://github.com/ryanmjacobs/github-copywriter/xx".yellow
+            # Repo refs (branches) to loop through
+            if options[:branches] != nil then
+                refs = options[:branches].map { |branch| "heads/#{branch}" }
+            else
+                refs = "heads/#{repo[:default_branch]}"
             end
 
-            # Stores updated files until we commit
-            # @modified_files is a hash {:path, :content}
-            @modified_files = Array.new
+            # Use all branches if we have the flag
+            if options[:all_branches] then
+                # Get all refs of type "heads"; remove leading "refs/"
+                refs = @client.refs(repo_name, "heads").map { |r| r[:ref].sub("refs/", "") }
+            end
 
-            # Loop through all of the tree's blobs
-            tree[:tree].each do |file|
-                next if file[:type] != "blob"
+            # Loop through each ref
+            refs.each do |ref|
+                puts "  #{ref}:".yellow
 
-                if Copywriter::Regex.accepted_name?(file[:path]) then
-                    # Grab file from repo
-                    file = @client.contents(repo_name, :path => file[:path])
-                    if file[:type] != "file" then raise "error: #{file_path} on #{repo} is not a file!" end
+                # Grab the *whole* tree
+                begin
+                    latest_commit = @client.ref(repo_name, ref).object
+                    root_tree     = @client.commit(repo_name, latest_commit.sha).commit.tree
+                    whole_tree    = @client.tree(repo_name, root_tree.sha, :recursive => true)
+                rescue Octokit::NotFound
+                    puts "error: ref #{ref} not found.".red
+                    exit
+                end
 
-                    # Update the copyright
-                    new_content = Copywriter::Regex.update_copyright(cur_year, Base64.decode64(file[:content]))
+                # Warn the user about truncation
+                if whole_tree[:truncated] then
+                    puts "    warning: tree truncated because number of items exceeded limit.".yellow
+                    puts "             If you feel like fixing this, see issue #xx".yellow
+                    puts "             http://github.com/ryanmjacobs/github-copywriter/xx".yellow
+                end
 
-                    # Add to list of files to commit, only if the file has changed
-                    if new_content != nil then
-                        @modified_files << {:path => file[:path], :content => new_content}
-                        puts "  #{file[:path]} is now up-to-date.".green
-                    else
-                        puts "  #{file[:path]} is already up-to-date."
+                # Stores updated files until we commit
+                # @modified_files is a hash {:path, :content}
+                @modified_files = Array.new
+
+                # Loop through all of whole_tree's blobs
+                whole_tree[:tree].each do |file|
+                    next if file[:type] != "blob"
+
+                    if Copywriter::Regex.accepted_name?(file[:path]) then
+                        # Grab file from repo
+                        file = @client.contents(repo_name, :path => file[:path], :ref => ref)
+                        if file[:type] != "file" then raise "error: #{file_path} on #{repo} is not a file!" end
+
+                        # Update the copyright
+                        new_content = Copywriter::Regex.update_copyright(cur_year, Base64.decode64(file[:content]))
+
+                        # Add to list of files to commit, only if the file has changed
+                        if new_content != nil then
+                            @modified_files << {:path => file[:path], :content => new_content}
+                            puts "    #{file[:path]} is now up-to-date.".green
+                        else
+                            puts "    #{file[:path]} is already up-to-date."
+                        end
                     end
                 end
-            end
 
-            # Commit stored up files
-            if @modified_files.size > 0 then
-                print "  Committing #{@modified_files.size} files..."
-                commit_files(repo_name, ref, "100644", @modified_files, COMMIT_MSG)
-                puts " done"
-            else
-                puts "  No files needed to be commited."
+                # Commit stored up files
+                if @modified_files.size > 0 then
+                    print "    Committing #{@modified_files.size} files..."
+                    commit_files(repo_name, ref, "100644", @modified_files, COMMIT_MSG)
+                    puts " done"
+                else
+                    puts "    No files needed to be commited."
+                end
             end
         end
     end
--- lib/github-copywriter/version.rb
+++ lib/github-copywriter/version.rb
@@ -8,6 +8,6 @@ class Copywriter
 
     # Current version
     # @return [String]
-    VERSION = "0.1.0".freeze
+    VERSION = "0.1.1".freeze
 
 end
